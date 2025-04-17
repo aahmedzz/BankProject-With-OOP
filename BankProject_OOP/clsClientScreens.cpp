ffbcf71735b1c308404006375b2bd6ec574a2313
Ahmed Nasr <an899645@gmail.com>
2025-04-17 11:11:14 +0200

    Added current user is username and date to all screens

--- BankProject_OOP/clsClientScreens.cpp
+++
@@ -1,1 +0,0 @@
-#include "clsClientScreens.h"
--- BankProject_OOP/clsScreen.h
+++ BankProject_OOP/clsScreen.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <iostream>
+#include "clsUser.h"
+#include "Global.h"
+#include "D://AHMED/Abu hahoud/My Liberaries/clsDate.h"
 using namespace std;
 
 class clsScreen
@@ -14,6 +17,10 @@ public:
             cout << "\n\t\t\t\t\t" << SubTitle;
         }
         cout << "\n\t\t\t\t     ___________________________________________\n\n";
+
+        cout << "\t\t\t\t     User: " << CurrentUser.UserName;
+        clsDate currentDate;
+        cout << "\n\t\t\t\t     Date: " << clsDate::DateToString(clsDate()) <<"\n\n";
     }
 };
 
